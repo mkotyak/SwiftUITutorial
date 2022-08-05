b93c079b23be4d7e0e0b4548bdf94545310a82e2
mkotyak <kotyak.maria@gmail.com>
2022-08-05 12:44:50 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maria Kotyak on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
